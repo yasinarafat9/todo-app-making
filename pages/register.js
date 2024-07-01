a5b84240e02be1d44516f577c27692da858f147f
unknown <yasinarafat9612@gmail.com>
2024-07-02 00:14:09 +0600

    Email password and google sign in added in Register page

--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,38 @@
-import React from "react";
+import { auth } from "@/firebase/firebase";
+import { createUserWithEmailAndPassword, updateProfile, getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 const RegisterForm = () => {
+
+    const provider = new GoogleAuthProvider();
+
+    const [username, setUsername] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+
+    const signupHandler = async () => {
+        if (!username || !email || !password) return;
+        try {
+            const user = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            await updateProfile(auth.currentUser, {
+                displayName: username
+            })
+            console.log(user);
+        } catch (error) {
+            console.log("An Error Found", error);
+        }
+
+    };
+
+    const signInWithGoogle = async () => {
+      const user = await signInWithPopup(auth, provider)
+      console.log(user);
+    }
+
     return (
         <main className="flex lg:h-[100vh]">
             <div className="w-full lg:w-[60%] p-8 md:p-14 flex items-center justify-center lg:justify-start">
@@ -13,37 +45,47 @@ const RegisterForm = () => {
                         </span>
                     </p>
 
-                    <div className="bg-black/[0.05] text-white w-full py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90 flex justify-center items-center gap-4 cursor-pointer group">
+                    <div onClick={signInWithGoogle}
+                        className="bg-black/[0.05] text-white w-full py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90 flex justify-center items-center gap-4 cursor-pointer group">
                         <FcGoogle size={22} />
                         <span className="font-medium text-black group-hover:text-white">
                             Login with Google
                         </span>
                     </div>
 
-                    <div className="mt-10 pl-1 flex flex-col">
-                        <label>Name</label>
-                        <input
-                            type="text"
-                            className="font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400"
-                        />
-                    </div>
-                    <div className="mt-10 pl-1 flex flex-col">
-                        <label>Email</label>
-                        <input
-                            type="text"
-                            className="font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400"
-                        />
-                    </div>
-                    <div className="mt-10 pl-1 flex flex-col">
-                        <label>Password</label>
-                        <input
-                            type="password"
-                            className="font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400"
-                        />
-                    </div>
-                    <button className="bg-black text-white w-44 py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90">
-                        Sign Up
-                    </button>
+                    <form onSubmit={(e) => e.preventDefault()}>
+                        <div className="mt-10 pl-1 flex flex-col">
+                            <label>Name</label>
+                            <input
+                                type="text"
+                                className="font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400"
+                                required
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+                        <div className="mt-10 pl-1 flex flex-col">
+                            <label>Email</label>
+                            <input
+                                type="email"
+                                className="font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400"
+                                required
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="mt-10 pl-1 flex flex-col">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                className="font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400"
+                                required
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <button onClick={signupHandler} className="bg-black text-white w-44 py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90">
+                            Sign Up
+                        </button>
+                    </form>
+
                 </div>
             </div>
             <div
